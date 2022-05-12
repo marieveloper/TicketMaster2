8f061449744740e2714bfc4a996d1c7cfca88fa3
marieveloper <marie.kufner@web.de>
2022-05-12 18:59:30 +0200

    First commit

---
+++ src/main/java/de/hohenheim/ticketmaster2/TicketMaster2Application.java
@@ -0,0 +1,13 @@
+package de.hohenheim.ticketmaster2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMaster2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketMaster2Application.class, args);
+    }
+
+}
---
+++ src/test/java/de/hohenheim/ticketmaster2/TicketMaster2ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hohenheim.ticketmaster2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketMaster2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
