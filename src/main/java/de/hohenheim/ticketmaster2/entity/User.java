e07dbd6422405c109990fe06be241a4a5c5b47c0
marieveloper <marie.kufner@web.de>
2022-05-25 09:18:00 +0200

    Created User entity

---
+++ src/main/java/de/hohenheim/ticketmaster2/entity/User.java
@@ -0,0 +1,44 @@
+package de.hohenheim.ticketmaster2.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer userId;
+
+    private String username;
+
+    private String password;
+
+    public User() {
+        // empty constructor for Hibernate
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
