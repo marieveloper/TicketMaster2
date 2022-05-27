e04c50b6837b23519dee60dcb6371cdb7c3f5e67
Annalena Schmid <st173100@stud.uni-stuttgart.de>
2022-05-27 15:49:05 +0200

    Lazy Annotation added
    Bootrun works

--- src/main/java/de/hohenheim/ticketmaster2/config/SecurityConfiguration.java
+++ src/main/java/de/hohenheim/ticketmaster2/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package de.hohenheim.ticketmaster2.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -17,6 +18,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
+    @Lazy
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
