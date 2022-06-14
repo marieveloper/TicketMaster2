de31dbf20ae1f88cf6a7b570831de5de2749c2ec
Kevin <st172192@stud.uni-stuttgart.de>
2022-06-14 09:06:43 +0200

    Create IncidentCategorization.java
    Create Enum

---
+++ src/main/java/de/hohenheim/ticketmaster2/enums/IncidentCategorization.java
@@ -0,0 +1,9 @@
+package de.hohenheim.ticketmaster2.enums;
+
+/**
+ * Enum for the categorization of the incidents
+ */
+public enum IncidentCategorization {
+    INACTIVITY, TECHNICALPROBLEMS, OTHER
+}
+
