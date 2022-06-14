824b7303479aec5c27fbd2e1dc9e60257e55ffb1
MariusHaueis <74296842+MariusHaueis@users.noreply.github.com>
2022-06-14 16:28:59 +0200

    Worked on login page

--- src/main/java/de/hohenheim/ticketmaster2/controller/HomeController.java
+++ src/main/java/de/hohenheim/ticketmaster2/controller/HomeController.java
@@ -1,13 +1,11 @@
 package de.hohenheim.ticketmaster2.controller;
 
-import de.hohenheim.ticketmaster2.entity.User;
 import de.hohenheim.ticketmaster2.service.TicketService;
 import de.hohenheim.ticketmaster2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -39,5 +37,6 @@ public class HomeController {
         model.addAttribute("message", "Und hier sehen Sie ein ModelAttribut");
         return "createTicket";
     }
+
 
 }
