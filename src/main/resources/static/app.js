c356e4e068a8eda0d16cdb406543846eff63d139
Marie <st173710@stud.uni-stuttgart.de>
2022-07-13 14:45:10 +0200

    changed Chat

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -61,8 +61,16 @@ function showGreeting(message) {
 
     var chatMessageTest = JSON.parse(message.body);
     var list = $("#messageArea");
+    var fillingElement = document.createElement("div");
+    var fillingRight = document.createElement("div");
+    fillingElement.classList.add("row");
+    fillingRight.classList.add("right");
     var messageElement = document.createElement('li');
-    messageElement.classList.add('chat-message');
+    if (chatMessageTest.author.id == author.id) {
+        messageElement.classList.add('chat-message-right');
+    } else {
+        messageElement.classList.add('chat-message');
+    }
     var usernameElement = document.createElement('span');
     usernameElement.classList.add('badge');
     usernameElement.classList.add('badge-primary');
@@ -73,7 +81,7 @@ function showGreeting(message) {
         var usernameText = document.createTextNode(chatMessageTest.author.username);
 
     }
-    var messageTimeElement= document.createElement('span');
+    var messageTimeElement = document.createElement('span');
     messageTimeElement.classList.add('timeClass');
     const date = new Date(chatMessageTest.creationTime);
     var dateStr =
@@ -81,7 +89,7 @@ function showGreeting(message) {
         ("00" + date.getDate()).slice(-2) + "/" +
         date.getFullYear() + " " +
         ("00" + date.getHours()).slice(-2) + ":" +
-        ("00" + date.getMinutes()).slice(-2) ;
+        ("00" + date.getMinutes()).slice(-2);
     var messageTimeText = document.createTextNode(dateStr);
     messageTimeElement.appendChild(messageTimeText);
     usernameElement.appendChild(usernameText);
@@ -89,12 +97,17 @@ function showGreeting(message) {
     messageElement.appendChild(messageTimeElement);
     var textElement = document.createElement('p');
     var messageText = document.createTextNode(chatMessageTest.text);
-
     textElement.appendChild(messageText);
-
     messageElement.appendChild(textElement);
-    console.log(messageElement);
-    list.append(messageElement);
+    if (chatMessageTest.author.username == author.username) {
+        fillingRight.appendChild(messageElement);
+        console.log(messageElement);
+        fillingElement.appendChild(fillingRight);
+    } else {
+        fillingElement.appendChild(messageElement);
+    }
+    console.log(fillingElement);
+    list.append(fillingElement);
     list.scrollTop(list.prop('scrollHeight'));
 
 
